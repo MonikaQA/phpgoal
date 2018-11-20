55766a7d29c3988b6e9439a9cc90be08fdbc3df2
cws <monika.k@cwsinotech.com>
2018-11-20 18:35:21 +0530

    php  first commit

--- form.php
+++ form.php
@@ -1,10 +1,12 @@
 <html>
 <body>
-<form action= "welcome.php" method="post">
+<form method="post" action="<?php
+htmlspecialchars($_server["PHP_self"]);?>
+
 Name: <input type="text" name="name"><br>
 Email: <input type="text" name="email"><br>
 <textarea name= "comment" rows="5" cols= "40"></textarea>
-<input type= "submit">
+//<input type= "submit">
 </form>
 </body>
 </html>
